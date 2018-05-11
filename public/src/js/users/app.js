f3064b475bb2936ced6fffe74a51f65e14c2ef13
Jason Stollman <Jason.Stollman@gmail.com>
2018-05-11 09:33:13 -0500

    End of Edit method and start of Delete Method

--- public/src/js/users/app.js
+++ public/src/js/users/app.js
@@ -49,7 +49,7 @@ function viewUser(id){
         var data = JSON.parse(xhr.response);
         var user = data.user;
         var app = document.getElementById('app');
-console.log(data);
+
         app.innerHTML=`
             <h2>${user.last_name}, ${user.first_name}</h2>    
             <table class="table">
@@ -62,7 +62,7 @@ console.log(data);
                 </tbody>
             </table>
             
-            <h3>Edit the Usser Record</h3>
+            <h3>Edit the User Record</h3>
             <form id="editUser" action="/api/users" method="put">
                 <input type="hidden" name="_id" value="${user._id}">
                 <div>
@@ -119,9 +119,96 @@ console.log(data);
                 object[key] = value;
             });
             xhr.send(JSON.stringify(object));
+            
+            xhr.onload = function(){
+                let data = JSON.parse(xhr.response);
+                if(data.success===true){
+                    viewIndex();
+                }
+            }
         });
 
     }
 }
+function createUser(){
+    var app = document.getElementById('app');
+    app.innerHTML = `<h2>Create a New User</h2>
+    <form id="createUser" action="/api/users" method="post">
+    <div>
+        <label for="username">Username</label>
+        <input 
+            type="text" 
+            name="username" 
+            id="username" >
+    </div>
+    <div>
+        <label for="email">Email</label>
+        <input 
+            type="text" 
+            name="email" 
+            id="email" >
+    </div>
+    <div>
+        <label for="first_name">First Name</label>
+        <input 
+            type="text" 
+            name="first_name" 
+            id="first_name" >
+    </div>
+    <div>
+        <label for="last_name">Last Name</label>
+        <input 
+            type="text" 
+            name="last_name" 
+            id="last_name" >
+    </div>
+
+    <input type="submit" value="Submit">
+    </form>
+    `;
+    
+    var createUser = document.getElementById('createUser');
+    createUser.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        var formData = new FormData(createUser);
+        var url = 'http://localhost:3000/api/users';
+        var xhr = new XMLHttpRequest();
+        xhr.open('Post', url);
+
+        xhr.setRequestHeader(
+            'Content-Type',
+            'application/json; charset=UTF-8'
+        );
+
+        var object = {};
+        formData.forEach(function(value, key){
+            object[key]=value;
+        });
+
+        xhr.send(JSON.stringify(object));
+        xhr.onload = function(){
+            let data = JSON.parse(xhr.response);
+            if(data.success===true){
+                viewIndex();
+            }
+        }
+
+    });
+}
 
 viewIndex();
+
+var hash = window.location.hash.substr(1);
+if(hash){
+    let chunks = hash.split('-');
+
+    if(chunks[0]=='edit'){
+        viewUser(chunks[1])
+    }
+
+    if(chunks[0]=='createUser'){
+        createUser();
+    }
+}
+

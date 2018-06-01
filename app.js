06dd81e750f2fabf007c9982fbe81b534951aa9e
Jason Stollman <Jason.Stollman@gmail.com>
2018-06-01 15:47:45 -0500

    Shameful hack aka stupid fix

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use(helmet());
 if(process.env.NODE_ENV==='production'){
   var config = require('../config.prod');
 }else{
-  var config = require('./config.dev');
+  //var config = require('./config.dev');
+  var config = require('../config.prod');
 }
 
 var passport = require('passport');
